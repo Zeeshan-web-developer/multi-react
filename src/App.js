ffa99fef1cbac1f0c3eeeada114b12f42fae0d0e
Zeeshan Ahmad <zeeshan>
2023-12-02 21:43:11 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+ import {Dine} from 'test-demo/src/Dine.js';
+
+function App() {
+  return (
+    <div >
+      
+       <h2>hiiii</h2>
+        
+     
+        
+    </div>
+  );
+}
+
+export default App;
